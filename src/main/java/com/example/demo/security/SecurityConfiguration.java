8c14c1c5d85b6cb4e0919e0ac335e2bd603c58e1
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2022-06-03 10:57:27 +0200

    inprement springSecurity

---
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/css/**", "/js/**", "/images/**", "/register", "/registerUser").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/")
+				.defaultSuccessUrl("/home")
+				.failureUrl("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true)
+				.permitAll()
+				.and()
+			.csrf()
+			.disable();
+	}
+}
