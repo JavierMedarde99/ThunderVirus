4ff3ac4fd542ccfb07d73de9585e45cc1b1a6424
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2022-06-16 13:20:18 +0200

    finish the comment

--- src/main/java/com/example/demo/controller/RegistroUsuarioController.java
+++ src/main/java/com/example/demo/controller/RegistroUsuarioController.java
@@ -1,20 +1,17 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
+
 import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.quartz.QuartzTransactionManager;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/demo/utils/FileUploadUtil.java
+++ src/main/java/com/example/demo/utils/FileUploadUtil.java
@@ -2,11 +2,6 @@ package com.example.demo.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 import org.springframework.web.multipart.MultipartFile;
 
