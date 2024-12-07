62986dfc64c0653c8d1c9065ff2b44dba5d19f54
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2024-12-07 11:43:27 +0100

    fix comentarios

--- src/main/java/com/example/demo/entity/Comentarios.java
+++ src/main/java/com/example/demo/entity/Comentarios.java
@@ -4,24 +4,25 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name="comentarios")
-@AllArgsConstructor
 @NoArgsConstructor
 public class Comentarios {
 
 	@Id
-	private long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	@ManyToOne
     @JoinColumn(name = "id_usu")
@@ -32,4 +33,11 @@ public class Comentarios {
 	private String comentario;
 	@Column(name = "fecha_coment")
 	private Timestamp fechaComent;
+
+	public Comentarios(Usuarios usuarios, Jugadores jugador, String comentario, Timestamp fechaComent){
+		this.usuarios = usuarios;
+		this.jugador = jugador;
+		this.comentario= comentario;
+		this.fechaComent = fechaComent;
+	}
 }
